596b6fe80df5c7f86658d1bf0439834846d27913
baysiregar1001 <baysiregar1001@gmail.com>
2022-08-16 11:14:39 +0700

    Project cleaning service-app

---
+++ lib/splash_screen.dart
@@ -0,0 +1,89 @@
+import 'package:cleaningservice_app/color_scheme.dart';
+import 'package:cleaningservice_app/main_page.dart';
+import 'package:flutter/material.dart';
+
+
+class Splashscreen extends StatelessWidget {
+  const Splashscreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: deepPurple400,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+        const SizedBox(
+          height: 60,
+        ),
+        const Text(
+          'Cleaning Service\nOnline',
+          style: TextStyle(
+            fontSize: 40,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+          textAlign: TextAlign.center,
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        const Text(
+          'Universitas Prima\nIndonesia',
+          style: TextStyle(
+            fontSize: 20,
+            color: Colors.white,
+            fontWeight: FontWeight.w500,
+          ),
+          textAlign: TextAlign.center,
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        Container(
+          height: 350,
+          width: MediaQuery.of(context).size.width,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/image/splash.png'),
+              fit: BoxFit.cover,
+              )
+          ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+          InkWell(
+            onTap: () => Navigator.of(context).push
+            (MaterialPageRoute(
+              builder: (context) {
+                return const MainPage();
+            },
+          ),
+        ),
+            child: Container(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 60,
+                  vertical: 30,
+                ),
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                  ),
+                  color: Colors.white,
+                ),
+                child: Text(
+                  'Masuk',
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.w600,
+                    color: deepPurple400,
+                  ),
+                ),
+              )
+        )],
+        )
+      ],)
+    );
+  }
+}
